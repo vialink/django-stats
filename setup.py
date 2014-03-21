f88e6dd4fd64f2aab0230eb24b1cb907f8df2cce
Marcelo Salhab Brogliato <msbrogli@vialink.com.br>
2014-03-21 20:53:50 +0000

    Missing setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='django-stats',
+    version='0.0.1beta',
+    author='Vialink',
+    author_email='vialink@vialink.com.br',
+    description='Generate statistics about view usage',
+    license='Proprietay Software',
+    packages=['django_stats'],
+)
