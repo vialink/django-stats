ab92c5f9281b7637432e456bcf5740274ac97068
Marcelo Salhab Brogliato <msbrogli@vialink.com.br>
2014-03-21 21:28:05 +0000

    Improving print of information and including stddev

--- django_stats/processors.py
+++ django_stats/processors.py
@@ -18,5 +18,11 @@ class RequestByViewProcessor(object):
         a = self.data.items()
         a.sort(key=lambda x: x[0])
         for x in a:
-            print x[0], unicode(x[1]), '50%:{0}'.format(x[1].percentile(0.5)), '90%:{0}'.format(x[1].percentile(0.9))
+            print x[0], unicode(x[1])
+            print '    [50%] {0}'.format(x[1].percentile(0.50))
+            print '    [75%] {0}'.format(x[1].percentile(0.75))
+            print '    [90%] {0}'.format(x[1].percentile(0.90))
+            print '    [95%] {0}'.format(x[1].percentile(0.95))
+            print '    [99%] {0}'.format(x[1].percentile(0.99))
+            print ''
 
--- django_stats/utils.py
+++ django_stats/utils.py
@@ -4,21 +4,44 @@ import bisect
 
 class TimeSeries(object):
     def __init__(self, history=False):
+        # Maximum value.
         self.max = None
+
+        # Minimum value.
         self.min = None
+
+        # Sum of all values.
         self.sum = 0
+
+        # Sum of all values squared.
+        self.sum2 = 0
+
+        # Number of records.
         self.qty = 0
-        self.values = []
+
+        # History of all values.
         self.history = history
+        self.values = []
     
     @property
     def average(self):
         return (1.0 * self.sum) / self.qty
 
+    @property
+    def variance(self):
+        mu = self.average
+        mu2 = (1.0 * self.sum2) / self.qty
+        return mu2 - mu*mu
+
+    @property
+    def stddev(self):
+        return self.variance ** 0.5
+
     def add(self, value):
         self.max = max(self.max, value) if self.max else value
         self.min = min(self.min, value) if self.min else value
         self.sum += value
+        self.sum2 += value*value
         self.qty += 1
         if self.history:
             bisect.insort(self.values, value)
@@ -32,5 +55,5 @@ class TimeSeries(object):
         return self.values[idx]
 
     def __unicode__(self):
-        return u'interval=[{0}, {1}] average={2} quantity={3}'.format(self.min, self.max, self.average, self.qty)
+        return u'interval=[{0}, {1}] average={2} stddev={3} quantity={4}'.format(self.min, self.max, self.average, self.stddev, self.qty)
 
